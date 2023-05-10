972a4c899fc2ad7bd28e655832b5f768b089b872
connorwarme <connor.warme@gmail.com>
2023-05-10 13:25:33 -0600

    Create category schema

---
+++ models/category.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+const Category = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+})
+
+CategorySchema.virtual("url").get(function() {
+  return `/category/${this._id}`
+})
+
+module.exports = mongoose.model("Category", CategorySchema)
