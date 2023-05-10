6a192d7cbbb852831c543b9c25f9ece3eb57344f
connorwarme <connor.warme@gmail.com>
2023-05-10 14:28:05 -0600

    Fix typo in const name

--- models/category.js
+++ models/category.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
-const Category = new Schema({
+const CategorySchema = new Schema({
   title: {
     type: String,
     required: true,
--- models/friends.js
+++ models/friends.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
-const Friends = new Schema({
+const FriendsSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
     ref: "User",
