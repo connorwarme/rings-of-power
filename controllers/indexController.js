e2f6e7b06d6efe5567ca90720f513f8d30a3984d
connorwarme <connor.warme@gmail.com>
2023-05-10 15:00:43 -0600

    Build simple signup, update route and controller

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -5,3 +5,13 @@ const User = require("../models/user")
 exports.login_get = asyncHandler(async (req, res, next) => {
   res.render("login", { title: "Login" })
 })
+exports.login_post = asyncHandler(async (req, res, next) => {
+  res.send("Not implemented yet: login POST")
+})
+
+exports.signup_get = asyncHandler(async (req, res, next) => {
+  res.render("signup", { title: "Signup" })
+})
+exports.signup_post = asyncHandler(async (req, res, next) => {
+  res.send("Not implemented yet: signup POST")
+})
--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,10 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/login', index_controller.login_get)
+router.post('/login', index_controller.login_post)
+
+router.get('/signup', index_controller.signup_get)
+router.post('/signup', index_controller.signup_post)
 
 
 module.exports = router;
