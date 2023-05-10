ba9d877c9b5755e014041a03ed6354434f74a680
connorwarme <connor.warme@gmail.com>
2023-05-10 12:25:17 -0600

    Fix typo in config

--- mongoConfig.js
+++ mongoConfig.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_URL, { userNewUrlParser: true })
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on("error", console.error.bind(console, "Mongo DB connection error"))
