e57d497bc4d56717382530107dd4755a6ec2de04
connorwarme <connor.warme@gmail.com>
2023-05-10 14:50:33 -0600

    Create simple login view

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,5 +3,5 @@ const asyncHandler = require("express-async-handler")
 const User = require("../models/user")
 
 exports.login_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: login GET")
+  res.render("login", { title: "Login" })
 })
