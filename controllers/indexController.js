d51234d94302f762f3786cf8f491cb8b5d618c1a
connorwarme <connor.warme@gmail.com>
2023-05-10 15:37:49 -0600

    Begin working on signup post fn

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require("express-async-handler")
+const { body, validationResult } = require("express-validator")
 
 const User = require("../models/user")
 
@@ -12,6 +13,35 @@ exports.login_post = asyncHandler(async (req, res, next) => {
 exports.signup_get = asyncHandler(async (req, res, next) => {
   res.render("signup", { title: "Signup" })
 })
-exports.signup_post = asyncHandler(async (req, res, next) => {
-  res.send("Not implemented yet: signup POST")
-})
+exports.signup_post = [
+  body("first_name", "Please add your first name.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("family_name", "Please add your family name.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("email", "Please add your email address.")
+    .trim()
+    .isLength({ min: 1 })
+    .isEmail()
+    .withMessage("Must be a valid email address!")
+    .escape(),
+  body("password", "Password required, must be at least 6 characters.")
+    .trim()
+    .isLength({ min: 6 })
+    .escape(),
+  body("confirm_password", "Passwords did not match.")
+    .trim()
+    .custom((value, { req }) => {
+      return value === req.body.password
+    })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req)
+    const salt = 12
+    
+  })
+]
