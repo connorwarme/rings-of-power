c2127a168e0d92a2f0717c11a67f716cba0097b3
connorwarme <connor.warme@gmail.com>
2023-05-15 19:43:17 -0600

    Fix typo

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(passport.session())
 // })
 
 app.use('/', indexRouter);
-app.use('/users', userRouter);
+app.use('/user', userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
